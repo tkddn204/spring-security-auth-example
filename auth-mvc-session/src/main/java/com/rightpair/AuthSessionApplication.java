15071b2596246c73dfd8c51190ef17ed62b9c8a8
rightpair <tkddn204@gmail.com>
2024-01-23 16:48:36 +0900

    auth-mvc-session 모듈 생성

---
+++ auth-mvc-session/src/main/java/com/rightpair/AuthSessionApplication.java
@@ -0,0 +1,11 @@
+package com.rightpair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSessionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSessionApplication.class, args);
+    }
+}
