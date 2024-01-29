16471539f22587fa2018d133da28ab41c0461650
rightpair <tkddn204@gmail.com>
2024-01-29 19:30:39 +0900

    auth-rest-api-jwt 모듈 추가

---
+++ auth-rest-api-jwt/src/main/java/com/rightpair/AuthJwtApplication.java
@@ -0,0 +1,12 @@
+package com.rightpair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthJwtApplication.class, args);
+    }
+}
