c703b48ea2f851a28820090372b90bc8b0994fbb
rightpair <tkddn204@gmail.com>
2024-01-30 19:43:49 +0900

    auth-rest-api-jwt 모듈 테스트 준비

---
+++ auth-rest-api-jwt/src/test/java/com/rightpair/AuthJwtApplicationTest.java
@@ -0,0 +1,15 @@
+package com.rightpair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AuthJwtApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
