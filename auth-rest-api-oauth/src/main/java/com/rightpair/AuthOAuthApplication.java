7dd0fa6b185bd82749f1ab826eec2d143a8de89b
rightpair <tkddn204@gmail.com>
2024-02-16 20:53:02 +0900

    auth-rest-api-oauth 모듈 추가

---
+++ auth-rest-api-oauth/src/main/java/com/rightpair/AuthOAuthApplication.java
@@ -0,0 +1,11 @@
+package com.rightpair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthOAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthOAuthApplication.class, args);
+    }
+}
